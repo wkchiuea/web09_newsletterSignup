8b7ae5285513f0be3e1fa3bbbf67bbe61548216b
wkchiuea <103587188+wkchiuea@users.noreply.github.com>
2022-06-23 15:26:30 +0800

    [update] add post request and parse data

--- app.js
+++ app.js
@@ -2,11 +2,21 @@
 const express = require("express");
 
 app = express();
-app.use(express.static(__dirname + "/public"));
+app.use(express.static("public")); // declare the path of the static files
+app.use(express.urlencoded({extended: true})); // parse requested data
 
 app.get("/", function(req, res) {
+    console.log("haha");
     res.sendFile(__dirname + "/signup.html");
 })
+
+app.post("/", function(req, res) {
+
+    var firstName = req.body.firstName;
+    var lastName = req.body.lastName;
+    var email = req.body.email;
+
+});
 
 app.listen(3000, function() {
     console.log("Server is running on port 3000.");
